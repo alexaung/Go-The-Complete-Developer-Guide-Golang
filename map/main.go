702a9c0535d5f59b093eca95c9d37b852930d482
Aung Myo Oo <aungmyooo@Aungs-MacBook-Pro.local>
2022-12-26 15:40:58 +0630

    map

---
+++ map/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main()  {
+	//var colors map[string]string
+
+	//colors := make(map[string]string)
+	colors := map[string]string{
+		"red": "#FF0000",
+		"white": "#FFFFFF",
+		"green": "#00FF00",
+	}
+
+	///delete(colors, "white")
+
+	printMap(colors)
+}
+
+func printMap(c map[string]string)  {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
+}
